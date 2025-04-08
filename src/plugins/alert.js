9d4c66b12f027e1cea6408196ad40c1928daa65a
AmelieKuo <amelie.kkuo@gmail.com>
2025-04-08 18:09:32 +0800

    feat: 完成 alert 元件

--- src/plugins/alert.js
+++ src/plugins/alert.js
@@ -1,4 +1,5 @@
 import { createApp, ref } from 'vue'
+import ElementPlus from 'element-plus'
 import AlertComponent from '@/components/Alert.vue'
 
 let instance = null
@@ -6,10 +7,11 @@ let instance = null
 export function setupAlert(app) {
   if (!instance) {
     const alertApp = createApp(AlertComponent)
+    alertApp.use(ElementPlus)
     const div = document.createElement('div')
     document.body.appendChild(div)
     instance = alertApp.mount(div)
   }
 
-  app.provide('$alert', instance.open)
+  app.provide('$alert', instance.handleOpen)
 }
