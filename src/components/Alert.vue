3fd93bea0d7e9a8405b7afe29ec79a89249c505f
AmelieKuo <amelie.kkuo@gmail.com>
2025-04-08 18:09:06 +0800

    feat: 完成 alert 元件

--- src/components/Alert.vue
+++ src/components/Alert.vue
@@ -2,6 +2,7 @@
 import { ref, defineProps, computed } from 'vue'
 import { CircleCheck, Warning, CircleClose } from '@element-plus/icons-vue'
 import CommonButton from '@/components/CommonButton.vue'
+import { fa } from 'element-plus/es/locales.mjs'
 
 // const prop = defineProps({
 //   visible: {
@@ -32,48 +33,71 @@ const alertOptions = ref({
   type: '',
   showCancel: false,
   visible: false,
+  timer: null,
 })
 
 const isVisible = ref(false)
+const resolveAction = ref(null)
 
-const open = (options = {}) => {
+let autoCloseTimer = null
+
+const handleOpen = (options = {}) => {
   return new Promise(async (resolve) => {
     alertOptions.value = { ...alertOptions.value, ...options }
+    alertOptions.value.timer = options.timer / 1000 || null
     isVisible.value = true
-    resolveAction = resolve
+    if (options.timer) {
+      autoCloseTimer && clearTimeout(autoCloseTimer)
+      autoCloseTimer = setTimeout(() => {
+        handleClose();
+      }, options.timer)
+    }
+
+    resolveAction.value = resolve
   })
 }
 
-defineExpose({ open })
+const handleConfirm = () => {
+  resolveAction.value?.(true)
+  isVisible.value = false
+  resolveAction.value = null
+}
+
+const handleClose = () => {
+  resolveAction.value?.(false)
+  isVisible.value = false
+  resolveAction.value = null
+}
+
+defineExpose({ handleOpen })
 
 </script>
 
 <template>
   <Teleport to="body">
-    <transition name="fade">
-      <el-dialog v-show="isVisible" align-center center :show-close="false" width="500" custom-class="alertStyle"
-        class="rounded-5px p-20px">
-        <div class="mt--16px flex flex-col items-center gap-14px">
-          <el-icon class="text-center" :size="100">
-            <CircleCheck v-if="type === 'success'" class="color-#a5dc86" />
-            <Warning v-if="type === 'warning'" class="color-#f8bb86" />
-            <CircleClose v-if="type === 'error'" class="color-#f27474" />
-          </el-icon>
-          <h4 class="font-700 text-24px color-text_dark">{{ title }}</h4>
-          <div v-if="content">
-            {{ content }}
-          </div>
+    <el-dialog v-model="isVisible" align-center center :show-close="false" width="500" custom-class="alertStyle"
+      class="rounded-5px p-20px relative">
+      <div class="mt--16px flex flex-col items-center gap-14px">
+        <div class="w-20%">
+          <CircleCheck v-if="alertOptions.type === 'success'" class="color-#a5dc86" />
+          <Warning v-if="alertOptions.type === 'warning'" class="color-#f8bb86" />
+          <CircleClose v-if="alertOptions.type === 'error'" class="color-#f27474" />
         </div>
-        <el-progress :percentage="100" :indeterminate="true" :duration="5" />
-        <template #footer>
-          <div class="mt-5px">
-            <CommonButton name="取消" size="large" />
-            <CommonButton type="danger" name="取消" size="large" />
-            <CommonButton type="edit" name="確定" size="large" />
-          </div>
-        </template>
-      </el-dialog>
-    </transition>
+        <h4 class="font-700 text-24px color-text_dark">{{ alertOptions.title }}</h4>
+        <div v-if="alertOptions.content">
+          {{ alertOptions.content }}
+        </div>
+      </div>
+      <template #footer>
+        <div class="mt-5px">
+          <CommonButton @click="handleClose" name="取消" size="large" />
+          <CommonButton @click="handleClose" type="danger" name="取消" size="large" />
+          <CommonButton @click="handleConfirm" type="edit" name="確定" size="large" />
+        </div>
+      </template>
+      <el-progress v-if="alertOptions.timer" :percentage="100" :indeterminate="true" :duration="alertOptions.timer"
+        :show-text="false" class="absolute bottom-0 left-0 w-full rotate-180" />
+    </el-dialog>
   </Teleport>
 </template>
 
