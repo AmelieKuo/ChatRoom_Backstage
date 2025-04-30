7e493146bf1f08b4349cc8b01a952bc2fdd51685
AmelieKuo <amelie.kkuo@gmail.com>
2025-04-30 16:36:33 +0800

    feat 增加首頁團隊

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,5 +1,40 @@
+<script setup>
+const devTeam = [
+  {
+    name: 'Amelie',
+    role: '前端工程師',
+    104: 'https://github.com/AmelieKuo',
+    github: 'https://github.com/AmelieKuo',
+    cake: 'https://github.com/AmelieKuo',
+    email: 'amelie.chihyu.kuo@gmail.com',
+    phone: '0978101246',
+    image: '/images/devTeam/1.png',
+  },
+  {
+    name: 'Neil',
+    role: '後端工程師',
+    104: 'https://github.com/AmelieKuo',
+    github: 'https://github.com/AmelieKuo',
+    cake: 'https://github.com/AmelieKuo',
+    email: 'amelie.chihyu.kuo@gmail.com',
+    phone: '0978101246',
+    image: '/images/devTeam/1.png',
+  }
+]
+
+</script>
+
 <template>
   <section>
-    Index
+    <h1 class="color-main_green font-bold text-h1 mb-8px w-fit">開發團隊</h1>
+    <div>
+      <ul>
+        <li>
+          <ul>
+            <li></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
   </section>
 </template>
